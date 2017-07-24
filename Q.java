213228e8f90066ba6b80a238624078f59894695a
shanmitha <shanmithasekar@gmail.com>
2017-07-24 15:50:07 +0530

    Create Q.java

---
+++ Q.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Q
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a%2==0)
+System.out.print("even");
+else
+System.out.print("odd");
+}
+}
